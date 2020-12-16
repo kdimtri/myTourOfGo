aa7286644f3c619c37c35a89ff53ec279a544505
Gitea <gitea@fake.local>
2020-12-16 05:22:44 +0400

    ex06

--- ex06.reader/ex06.reader.go
+++ ex06.reader/ex06.reader.go
@@ -4,16 +4,23 @@ Implement a Reader type that emits an infinite stream of the ASCII character 'A'
 */
 package main
 
-import "golang.org/x/tour/reader"
+import (
+	//"io"
+
+	"golang.org/x/tour/reader"
+)
 
 //MyReader fils any byte slice with ASCII character 'A'
 type MyReader struct{}
 
 func (r MyReader) Read(b []byte) (int, error) {
+	if len(b) == 0 {
+		return 0, nil
+	}
 	for i := range b {
 		b[i] = byte('A')
 	}
-	return len(b), nil
+	return len(b), nil //shouldn't i return here an error="io.EOF"?
 }
 
 // TODO: Add a Read([]byte) (int, error) method to MyReader.
